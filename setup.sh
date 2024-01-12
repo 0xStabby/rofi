aa4bb334d51527f00de96756bc7ad20513df7fca
Stabby <0xStabby@protonmail.com>
2024-01-12 17:10:26 -0600

    added setup.sh script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# get rofi themes
+git clone --depth=1 https://github.com/adi1090x/rofi.git rofi-repo
+cd rofi-repo
+chmod +x setup.sh
+./setup.sh
+# cleanup uneeded rofi-repo
+cd ../
+rm -rf rofi-repo
